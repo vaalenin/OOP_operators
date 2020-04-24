aac372b53b2cc77ed58a896b3e95ad780e8037f4
vaalenin <vaalenin@gmail.com>
2020-04-24 09:49:32 +0300

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -3,5 +3,14 @@
 class Matrix
 {
 public:
-
+  Matrix (int v1, int v2);
+  ~Matrix () = default;
+  
+  Matrix operator+ (const Matrix& m);
+  Matrix& operator ++ (int);
+  
+  operator int ();
+private:
+  int val1;
+  int val2;
 };
