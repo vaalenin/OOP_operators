1d7197190b12f7f295cb73256fb4eed06647626f
vaalenin <vaalenin@gmail.com>
2020-04-24 09:59:40 +0300

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,1 +1,30 @@
+#include "Matrix.h"
 
+Matrix::Matrix(int v1, int v2) :
+  val1 (v1), val2 (v2)
+{
+}
+
+Matrix Matrix::operator+ (const Matrix& m)
+{
+  int tmpVal1 = this->val1;
+  tmpVal1 += m.val1;
+  
+  int tmpVal2 = this->val2;
+  tmpVal2 += m.val2;
+  
+  return Matrix (tmpVal1, tmpVal2);
+}
+
+Matrix& operator ++ (int)
+{
+  ++this->val1;
+  ++this->val2;
+  
+  return *this;
+}
+
+operator int ()
+{
+  return this->val1 + this->val2;
+}
