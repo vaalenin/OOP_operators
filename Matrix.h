ca501c698e7dafc167fb0d440c8ed3420b56e27c
vaalenin <vaalenin@gmail.com>
2020-04-25 21:17:43 +0300

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -2,8 +2,10 @@
 
 #include <iostream>
 
+//дружественный оператор вывода в поток 
 std::ostream& operator << (std::ostream& os, const Matrix& m)
 
+//дружественная функция
 void PrintMatrix (const Matrix& m);
 
 class Matrix
@@ -12,30 +14,38 @@ public:
   Matrix (int v1, int v2);
   ~Matrix () = default;
   
+  //бинарные операторы
   Matrix operator+ (const Matrix& m);
   //Matrix operator- (const Matrix& m);
   //Matrix operator* (const Matrix& m);
   //Matrix operator/ (const Matrix& m);
+  
+  //унарные операторы
   Matrix& operator ++ (int);
   //Matrix& operator -- (int);
   //Matrix& operator ++ ();
   //Matrix& operator -- ();
   
+  //оператор присваивания
   Matrix& operator = (const Matrix& m);
   
+  //логические операторы
   bool operator > (const Matrix& m);
   //bool operator < (const Matrix& m);
   //bool operator >= (const Matrix& m);
   //bool operator <= (const Matrix& m);
   //bool operator == (const Matrix& m);
   
+  //оператор вычисления объекта как функции
   int operator () ();
   int& operator [](int index);
   
+  //оператор привидения типов
   operator int ();
   
   friend std::ostream& operator << (std::ostream& os, const Matrix& dt);
   frined void PrintMatrix (const Matrix& dt);
+  
 private:
   int val1;
   int val2;
